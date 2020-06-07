746dceef41d8f38356e1c15130a3c383bd476639
Kohei Yamaguchi <fix7211@gmail.com>
2020-06-07 21:31:12 +0900

    Add for statement

--- 711cc.h
+++ 711cc.h
@@ -60,6 +60,7 @@ typedef enum {
     ND_ASSIGN,      // =
     ND_RETURN,      // "return"
     ND_IF,          // "if"
+    ND_FOR,         // "for"
     ND_BLOCK,       // { ... }
     ND_EXPR_STMT,   // Expression statement
     ND_VAR,         // Variable
@@ -75,10 +76,12 @@ struct Node {
     Node *lhs;      // Left-hand node
     Node *rhs;      // Right-hand node
 
-    // "if" statement
+    // "if" or "for" statement
     Node *cond;
     Node *then;
     Node *els;
+    Node *init;
+    Node *inc;
 
     // Block
     Node *body;
