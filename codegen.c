0e37bf0ad6bbafdc78b042ec8254634ac321f86c
Kohei Yamaguchi <fix7211@gmail.com>
2020-06-08 01:57:44 +0900

    Add unary & and *

--- codegen.c
+++ codegen.c
@@ -14,11 +14,17 @@ static char *reg(int idx) {
     return r[idx];
 }
 
+static void gen_expr(Node *node);
+
 // Compute the absolute address of a given node.
 // It's an error if a given node does not reside in memory.
 static void gen_addr(Node *node) {
-    if (node->kind == ND_VAR) {
+    switch (node->kind) {
+    case ND_VAR:
         printf("  lea -%d(%%rbp), %s\n", node->var->offset, reg(top++));
+        return;
+    case ND_DEREF:
+        gen_expr(node->lhs);
         return;
     }
 
@@ -43,6 +49,13 @@ static void gen_expr(Node *node) {
     case ND_VAR:
         gen_addr(node);
         load();
+        return;
+    case ND_DEREF:
+        gen_expr(node->lhs);
+        load();
+        return;
+    case ND_ADDR:
+        gen_addr(node->lhs);
         return;
     case ND_ASSIGN:
         gen_expr(node->rhs);
