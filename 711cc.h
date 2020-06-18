afa56cb86d7db3e897d69d39c14ffe4c04b2c1ab
Kohei Yamaguchi <fix7211@gmail.com>
2020-06-18 14:43:46 +0900

    Handle function argument type conversion

--- 711cc.h
+++ 711cc.h
@@ -121,6 +121,7 @@ struct Node {
 
     // Function call
     char *funcname;
+    Type *func_ty;
     Var **args;
     int nargs;
 
--- tests/tests.c
+++ tests/tests.c
@@ -64,6 +64,10 @@ int fib(int x) {
 int *g1_ptr() { return &g1; }
 char int_to_char(int x) { return x; }
 
+int div_long(long a, long b) {
+    return a / b;
+}
+
 int main() {
     assert(0, 0, "0");
     assert(42, 42, "42");
@@ -390,6 +394,7 @@ int main() {
 
     assert(3, *g1_ptr(), "*g1_ptr()");
     assert(5, int_to_char(261), "int_to_char(261)");
+    assert(-5, div_long(-10, 2), "div_long(-10, 2)");
 
     printf("OK\n");
     return 0;
