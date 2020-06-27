4fa6b777d6a9c88ffaf2624cb8f8178e3dc9a7bc
Kohei Yamaguchi <fix7211@gmail.com>
2020-06-27 13:43:09 +0900

    Add global initializer for scalar and string

--- tests/tests.c
+++ tests/tests.c
@@ -4,16 +4,22 @@
 
 int printf();
 int exit();
+int strcmp(char *p, char *q);
 
 int g1, g2[4];
 
 typedef int MyInt, MyInt2[4];
 
+char g3 = 3;
+short g4 = 4;
+int g5 = 5;
+long g6 = 6;
+
 int assert(int expected, int actual, char *code) {
     if (expected == actual) {
-        printf("%s => %d\n", code, actual);
+        printf("%s => %ld\n", code, actual);
     } else {
-        printf("%s => %d expected but got %d\n", code, expected, actual);
+        printf("%s => %ld expected but got %ld\n", code, expected, actual);
         exit(1);
     }
 }
@@ -657,6 +663,11 @@ int main() {
     assert(1, ({ typedef struct {int a,b;} T; T x={1,2}; T y=x; y.a; }), "({ typedef struct {int a,b;} T; T x={1,2}; T y=x; y.a; })");
     assert(5, ({ typedef struct {int a,b,c,d,e,f;} T; T x={1,2,3,4,5,6}; T y; y=x; y.e; }), "({ typedef struct {int a,b,c,d,e,f;} T; T x={1,2,3,4,5,6}; T y; y=x; y.e; })");
     assert(2, ({ typedef struct {int a,b;} T; T x={1,2}; T y, z; z=y=x; z.b; }), "({ typedef struct {int a,b;} T; T x={1,2}; T y, z; z=y=x; z.b; })");
+
+    assert(3, g3, "g3");
+    assert(4, g4, "g4");
+    assert(5, g5, "g5");
+    assert(6, g6, "g6");
 
     printf("OK\n");
     return 0;
