b1784a34e39790fe809f824abc9e4155ad3e5bbb
Kohei Yamaguchi <fix7211@gmail.com>
2020-07-24 10:56:50 +0900

    Add examples

---
+++ examples/fib.c
@@ -0,0 +1,14 @@
+int printf();
+
+int fib(int n) {
+     if (n == 0) return 0;
+     if (n == 1) return 1;
+     return fib(n - 1) + fib(n - 2);
+ }
+
+ int main() {
+     int c = 20;
+     for (int i = 0; i < c; i++)
+         printf("fib(%d): %d\n", i, fib(i));
+ }
+
